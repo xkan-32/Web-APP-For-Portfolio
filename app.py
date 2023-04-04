687b4b250a86397b86722159cbb6961039c742c1
xkan-32 <121345057+xkan-32@users.noreply.github.com>
2023-04-04 23:22:47 +0900

    年月日,一覧表示追加

--- app.py
+++ app.py
@@ -1,34 +1,46 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, render_template, request
 import mysql.connector
+import datetime
 
 app = Flask(__name__)
 
-# MySQL database connection information
-host = "hostname"
-user = "username"
-password = "password"
-database = "water_bill_tracker"
-
-# Connect to the database
-conn = mysql.connector.connect(
-    host=host,
-    user=user,
-    password=password,
-    database=database
-)
-cursor = conn.cursor()
-
 @app.route("/")
 def index():
-    return render_template("index.html")
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="user",
+        password="password",
+        database="water_bill_tracker"
+    )
+    
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM water_data")
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    
+    return render_template("index.html", data=data)
 
 @app.route("/add_data", methods=["POST"])
 def add_data():
     water_bill = request.form["water_bill"]
     water_usage = request.form["water_usage"]
-    cursor.execute("INSERT INTO water_data (water_bill, water_usage) VALUES (%s, %s)", (water_bill, water_usage))
+    year_month = datetime.datetime.now().strftime("%Y-%m") # 追加するコード
+    
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="user",
+        password="password",
+        database="water_bill_tracker"
+    )
+    
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO water_data (year_month, water_bill, water_usage) VALUES (%s, %s, %s)", (year_month, water_bill, water_usage)) # 修正するコード
     conn.commit()
-    return redirect("/")
+    cursor.close()
+    conn.close()
+    
+    return "Data added successfully."
 
 if __name__ == "__main__":
     app.run(debug=True)
